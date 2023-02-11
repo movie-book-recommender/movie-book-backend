c7427fd789ad6f0fa5cbf018fea23df9dd566002
Katri <katri.rytkonen@outlook.com>
2023-02-11 16:06:26 +0200

    Tests added

---
+++ main/tests/test_extentions.py
@@ -0,0 +1,12 @@
+import unittest
+import main.extentions as extentions
+
+class TestExtentions(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
+
+if __name__ == '__main__':
+    unittest.main()
